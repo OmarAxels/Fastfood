da72dc14ba79e6be6d081772fe3c0e670dc2b80d
omar.axelsson@tr.is <omar.axelsson@tr.is>
2025-07-17 16:51:27 +0000

    Update Vercel frontend URL in CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -152,7 +152,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
-        "https://fastfood-xi-pied.vercel.app"  # Vercel frontend
+        "https://fastfood-zszi.vercel.app/"  # Vercel frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
